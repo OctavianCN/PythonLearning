3fb3e6a13315a66a2d8f1fd518b9d32aa091b0e7
OctavianCN <octavian.cordescu@yahoo.com>
2022-08-26 10:32:41 +0300

    Commit Section: 10 L: 144 - 26 Aug 2022

---
+++ Modules, Packages and Namespaces/NamespacePackages.py
@@ -0,0 +1,10 @@
+"""
+Namespace packages are package-like directories:
+    - may contain modules
+    - may contain nested regular packages
+    - may contain nested namespace packages
+    - but cannot contain __init__.py
+Thes directories are implicitly made into these special types of packages
+PEP 420 - for more info
+
+"""
